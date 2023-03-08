2e78bbb9b67ffba515b07453b14f3a9b625f02f9
Ahmed <ahmdadl563@gmail.com>
2023-03-08 02:22:57 +0200

    Create supplies and supply_categries tables

---
+++ db/migrate/20230308002059_create_supplies.rb
@@ -0,0 +1,11 @@
+class CreateSupplies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :supplies do |t|
+      t.string :name, limit: 50, null: false
+      t.integer :price, null: false
+      t.references :supply_category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_211247) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_002059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,7 +77,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_211247) do
     t.index ["customer_id"], name: "index_sites_on_customer_id"
   end
 
+  create_table "supplies", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.integer "price", null: false
+    t.bigint "supply_category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supply_category_id"], name: "index_supplies_on_supply_category_id"
+  end
+
+  create_table "supply_categories", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "customer_contracts", "customers"
   add_foreign_key "generators", "customers"
   add_foreign_key "sites", "customers"
+  add_foreign_key "supplies", "supply_categories"
 end
