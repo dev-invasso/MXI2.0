951781db8ab00fa7d7ed51b51f068fff594f2df7
sediq <sediq@invasso.com>
2023-03-10 02:13:42 +0430

    create reports model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_212600) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_214125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,36 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_212600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_report_a_problems_on_user_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.decimal "hotel_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "food_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "car_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "msw_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "disposal_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "transport_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "car_count_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "billing_cost", precision: 10, scale: 2, default: "0.0"
+    t.integer "portajon_q", default: 0
+    t.integer "forklift_q", default: 0
+    t.integer "truck_rental_q", default: 0
+    t.integer "supply_trailer_q", default: 0
+    t.integer "tag_along_q", default: 0
+    t.decimal "mile_cost", precision: 10, scale: 2, default: "0.0"
+    t.decimal "event_billing_cost", precision: 10, scale: 2, default: "0.0"
+    t.text "supply_note"
+    t.text "car_note"
+    t.decimal "total_mile_cost", precision: 10, scale: 2, default: "0.0"
+    t.string "invoice"
+    t.boolean "is_bond", default: true
+    t.integer "forklift_rental_q", default: 0
+    t.integer "car_rental_q", default: 0
+    t.integer "trash_roll_off", default: 0
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_reports_on_event_id"
   end
 
   create_table "sites", force: :cascade do |t|
@@ -231,6 +261,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_212600) do
   add_foreign_key "events", "customers"
   add_foreign_key "events", "sites"
   add_foreign_key "report_a_problems", "users"
+  add_foreign_key "reports", "events"
   add_foreign_key "sites", "customers"
   add_foreign_key "supplies", "supply_categories"
   add_foreign_key "supply_assignments", "supplies"
