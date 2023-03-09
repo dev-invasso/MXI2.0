f27dd00a60bf0fb5d4b3aaf04cb01d6ba911dfac
sediq <sediq@invasso.com>
2023-03-10 04:19:03 +0430

    create meetup model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_234439) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_234817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -117,6 +117,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_234439) do
     t.index ["designated_facility_id"], name: "index_manifests_on_designated_facility_id"
     t.index ["manifest_type_id"], name: "index_manifests_on_manifest_type_id"
     t.index ["transporter_id"], name: "index_manifests_on_transporter_id"
+  end
+
+  create_table "meetups", force: :cascade do |t|
+    t.string "address", limit: 50, null: false
+    t.datetime "date_time", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_meetups_on_event_id"
   end
 
   create_table "report_a_problems", force: :cascade do |t|
@@ -353,6 +362,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_234439) do
   add_foreign_key "manifests", "designated_facilities"
   add_foreign_key "manifests", "manifest_types"
   add_foreign_key "manifests", "transporters"
+  add_foreign_key "meetups", "events"
   add_foreign_key "report_a_problems", "users"
   add_foreign_key "report_line_items", "reports"
   add_foreign_key "report_other_item_details", "reports"
