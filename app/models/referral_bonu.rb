413ffece0f19771cdf7c4e96db393731fd3057a2
sediq <sediq@invasso.com>
2023-03-15 01:03:01 +0430

    associate referal_bonus with staff_members

--- app/models/referral_bonu.rb
+++ app/models/referral_bonu.rb
@@ -1,2 +1,4 @@
 class ReferralBonu < ApplicationRecord
+  belongs_to :refered_staff, class_name: 'StaffMember', foreign_key: 'refered_staff_id'
+  belongs_to :referer_staff, class_name: 'StaffMember', foreign_key: 'referer_staff_id'
 end
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,4 +1,9 @@
 class StaffMember < ApplicationRecord
-    has_many: event_staff_lists
-    has_many: events, through: :event_staff_lists
+    has_many :event_staff_lists
+    has_many :events, through: :event_staff_lists
+    belongs_to :staff_group
+    has_many :staff_job_capabilities
+    has_many :job_capabilities, through: :staff_job_capabilities
+    has_many :referral_bonuses_as_refered_staff, class_name: 'ReferralBonus', foreign_key: 'refered_staff_id'
+    has_many :referral_bonuses_as_referer_staff, class_name: 'ReferralBonus', foreign_key: 'referer_staff_id'
 end
