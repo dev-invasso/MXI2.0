f0a0e4b42d80a3057b5928c9e0702f9ae73ae271
sediq <sediq@invasso.com>
2023-03-14 23:59:27 +0430

    associate event and site with assignments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+    has_many :supply_assignments, as: :assignable
+    has_many :supplies, through: :supply_assignments
+    has_many :equipment_assignments, as: :equipmentable
+    has_many :equipment, through: :equipment_assignments
     belongs_to :customer
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,8 @@ class Site < ApplicationRecord
   belongs_to :customer
   has_many :supply_assignments, as: :assignable
   has_many :supplies, through: :supply_assignments
+  has_many :equipment_assignments, as: :equipmentable
+  has_many :equipment, through: :equipment_assignments
+  has_many :events
+  
 end
