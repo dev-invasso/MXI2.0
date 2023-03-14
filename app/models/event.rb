59d484fd9c1f1ab01dd5a70a20fa1f8cb674c3ca
sediq <sediq@invasso.com>
2023-03-15 00:34:51 +0430

    associate event with staff_members

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,8 @@ class Event < ApplicationRecord
     has_many :supplies, through: :supply_assignments
     has_many :equipment_assignments, as: :equipmentable
     has_many :equipment, through: :equipment_assignments
+    has_many :event_staff_lists
+    has_many :staff_members, through: :event_staff_lists
+    
     belongs_to :customer
 end
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,2 +1,4 @@
 class StaffMember < ApplicationRecord
+    has_many: event_staff_lists
+    has_many: events, through: :event_staff_lists
 end
